3ab78c3464015663bde12c849d9bbf9f4c77d17a
Sheena Anagnostopoulos <sheenaanagnostopoulos@gmail.com>
2014-11-25 18:23:28 -0800

    created slideshow

---
+++ slideshow/static/script.js
@@ -0,0 +1,32 @@
+var form, input, grid;
+
+form = document.getElementById("image-form");
+input = document.getElementById("image-url");
+grid = document.getElementById("grid");
+
+var getImage = function(){
+	var container, img, remove;
+	event.preventDefault();
+	var text = input.value;
+	if(text === "") {
+		alert("Missing url");
+		return;
+	}
+	text.value = "";
+	container = document.createElement("div");
+	container.setAttribute("class", "image-container");
+	img = document.createElement("img");
+	img.setAttribute("src", text);
+	remove = document.createElement("button");
+	remove.innerHTML = "Delete image";
+	container.appendChild(img);
+	container.appendChild(remove);
+	grid.appendChild(container);
+
+	remove.addEventListener("click", function(){
+		grid.removeChild(container);
+	});
+
+};
+
+form.addEventListener("submit", getImage);
